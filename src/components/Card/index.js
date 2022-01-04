f5ed09d91d906f20b29faa3ffecc376f8df88e6d
hique023 <pedroviski10@hotmail.com>
2022-01-04 14:00:08 -0300

    Inserido props para o componente Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,13 +4,13 @@ import React from "react";
 // Styles
 import "./styles.css";
 
-export default function Card() {
+export default function Card({ name, email, phone, website }) {
   return (
     <div className="contentCard">
-      <h2>Nome: </h2>
-      <h2>Email: </h2>
-      <h2>Telefone: </h2>
-      <h2>Website: </h2>
+      <h2>Nome: {name}</h2>
+      <h2>Email: {email}</h2>
+      <h2>Telefone: {phone}</h2>
+      <h2>Website: {website}</h2>
     </div>
   );
 }
